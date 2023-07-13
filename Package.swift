cf6e2b0ba8e9faef6feaff6fef09a028af1be50f
Batuhan Göbekli <batuhan@getbaraka.com>
2023-07-13 19:20:46 +0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "Segment-Singular",
+    platforms: [
+        .iOS(.v8)
+    ],
+    products: [
+        .library(
+            name: "Segment-Singular",
+            targets: ["Segment-Singular"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/segmentio/analytics-ios.git", from: "4.1.0"),
+        .package(url: "https://github.com/singular-labs/singular-sdk-ios.git", from: "1.5.0")
+    ],
+    targets: [
+        .target(
+            name: "Segment-Singular",
+            dependencies: [
+                .product(name: "Analytics", package: "analytics-ios"),
+                .product(name: "SingularSDK", package: "singular-sdk-ios")
+            ],
+            path: "Segment-Singular-iOS",
+            sources: [
+                "Segment-Singular-iOS" // Specify the folder containing your Swift source files
+            ]
+        )
+    ]
+)
