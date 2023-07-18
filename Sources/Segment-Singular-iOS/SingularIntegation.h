86145c6366f16d7c41356f0fec9b021fa26e4ca8
Batuhan Göbekli <batuhan@getbaraka.com>
2023-07-18 12:06:09 +0300

    Update SingularIntegation.h

--- Sources/Segment-Singular-iOS/SingularIntegation.h
+++ Sources/Segment-Singular-iOS/SingularIntegation.h
@@ -7,7 +7,14 @@
 //
 
 #import <Foundation/Foundation.h>
+
+#if defined(__has_include) && __has_include(<Analytics/SEGIntegration.h>)
+#import <Analytics/SEGIntegration.h>
+#elif defined(__has_include) && __has_include(<Segment/SEGIntegration.h>)
 #import <Segment/SEGIntegration.h>
+#else
+#import "SEGIntegration.h"
+#endif
 
 NS_ASSUME_NONNULL_BEGIN
 
